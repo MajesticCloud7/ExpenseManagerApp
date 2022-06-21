46706327f8a7d112d163aba2a22748759b397d4e
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-21 22:47:45 +0300

    Add database connection

--- ExpenseManager/MainWindow.xaml.cs
+++ ExpenseManager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using ExpenseManager.DatabaseModels;
+using MongoDB.Driver;
+using System.Windows;
 
 namespace ExpenseManager
 {
@@ -7,15 +9,23 @@ namespace ExpenseManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string ConnectionString = "mongodb://127.0.0.1:27017";
+        private const string DatabaseName = "ExpenseManagerDB";
+        private readonly IMongoDatabase Database;
+
         public MainWindow()
         {
             InitializeComponent();
+            var mongoClient = new MongoClient(ConnectionString);
+            Database = mongoClient.GetDatabase(DatabaseName);
         }
 
         private void SignUpButton_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new SignUpForm();
-            dialog.Owner = this;
+            var dialog = new SignUpForm
+            {
+                Owner = this
+            };
             dialog.ShowDialog();
         }
     }
