af97d3c3c7d4981413fbec33116ccfa9c9720c45
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-21 22:41:42 +0300

    EM-143 Create sign up form

---
+++ ExpenseManager/SignUpForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ExpenseManager
+{
+    /// <summary>
+    /// Interaction logic for SignUpForm.xaml
+    /// </summary>
+    public partial class SignUpForm : Window
+    {
+        public SignUpForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
