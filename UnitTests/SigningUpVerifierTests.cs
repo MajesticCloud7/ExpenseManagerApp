e8eee38423bd99294f5b0f7365579ec15845a08f
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-22 20:38:50 +0300

    EM-142 Add test project and SigningUpVerifierTests

---
+++ UnitTests/SigningUpVerifierTests.cs
@@ -0,0 +1,74 @@
+using ExpenseManager;
+using ExpenseManager.DatabaseModels;
+
+namespace UnitTests
+{
+    public class SigningUpVerifierTests
+    {
+        [Theory]
+        [MemberData(nameof(VerifyTestCases))]
+        public void Verify(string email, string username, string firstPassword, string secondPassword, List<User> existingUsers, bool expected)
+        {
+            var actual = SigningUpVerifier.Verify(email, username, firstPassword, secondPassword, existingUsers);
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> VerifyTestCases =>
+            new List<object[]>
+            {
+                // all empty
+                new object[]
+                {
+                    "", "", "", "",
+                    new List<User>(),
+                    false
+                },
+
+                // one empty
+                new object[]
+                {
+                    "", "user", "password", "password",
+                    new List<User>(),
+                    false
+                },
+
+                // existing email
+                new object[]
+                {
+                    "email", "user", "password", "password",
+                    new List<User>
+                    {
+                        new User("email", "username", "password")
+                    },
+                    false
+                },
+
+                // existing username
+                new object[]
+                {
+                    "email", "user", "password", "password",
+                    new List<User>
+                    {
+                        new User("emailAddress", "user", "password")
+                    },
+                    false
+                },
+                
+                // unmatching passwords
+                new object[]
+                {
+                    "email", "user", "password1", "password2",
+                    new List<User>(),
+                    false
+                },
+                
+                // valid new user
+                new object[]
+                {
+                    "email", "user", "password", "password",
+                    new List<User>(),
+                    true
+                },
+            };
+    }
+}
