241afa04215243e20bf9c5ea1b614a36d81e2d1b
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-22 21:21:50 +0300

    EM-141 Add SigningInVerifierTests

---
+++ ExpenseManager/SigningInVerifier.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseManager.DatabaseModels;
+using System.Collections.Generic;
+
+namespace ExpenseManager
+{
+    public static class SigningInVerifier
+    {
+        public static bool Verify(string username, string password, List<User> existingUsers)
+        {
+            return true;
+        }
+    }
+}
