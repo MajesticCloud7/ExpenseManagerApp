23821629a7d4eb5001c556a993e1b8df31be9404
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-22 21:21:50 +0300

    EM-141 Add SigningInVerifierTests

---
+++ UnitTests/SigningInVerifierTests.cs
@@ -0,0 +1,69 @@
+﻿using ExpenseManager;
+using ExpenseManager.DatabaseModels;
+
+namespace UnitTests
+{
+    public class SigningInVerifierTests
+    {
+        [Theory]
+        [MemberData(nameof(VerifyTestCases))]
+        public void Verify(string username, string password, List<User> existingUsers, bool expected)
+        {
+            var actual = SigningInVerifier.Verify(username, password, existingUsers);
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> VerifyTestCases =>
+            new List<object[]>
+            {
+                // both empty
+                new object[]
+                {
+                    "", "",
+                    new List<User>(),
+                    false
+                },
+
+                // one empty
+                new object[]
+                {
+                    "user", "",
+                    new List<User>(),
+                    false
+                },
+
+                // username does not exist
+                new object[]
+                {
+                    "user", "password",
+                    new List<User>
+                    {
+                        new User("email", "username", "password")
+                    },
+                    false
+                },
+
+                // wrong password
+                new object[]
+                {
+                    "user", "pass",
+                    new List<User>
+                    {
+                        new User("email", "user", "password")
+                    },
+                    false
+                },
+                
+                // valid user
+                new object[]
+                {
+                    "user", "password",
+                    new List<User>
+                    {
+                        new User("email", "user", "password")
+                    },
+                    true
+                },
+            };
+    }
+}
