5593a31d5100ee33359fccde5133a229da134efe
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-23 21:46:52 +0300

    EM-147 Implement expense record creation

---
+++ ExpenseManager/DatabaseModels/Record.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ExpenseManager.DatabaseModels
+{
+    public class Record
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public RecordType RecordType { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+
+        public Record(string userId, RecordType recordType, double amount, string description)
+        {
+            UserId = userId;
+            RecordType = recordType;
+            Amount = amount;
+            Description = description;
+        }
+    }
+
+    public enum RecordType
+    {
+        Expense,
+        Income
+    }
+}
--- ExpenseManager/MainWindow.xaml.cs
+++ ExpenseManager/MainWindow.xaml.cs
@@ -12,15 +12,17 @@ namespace ExpenseManager
         private const string ConnectionString = "mongodb://127.0.0.1:27017";
         private const string DatabaseName = "ExpenseManagerDB";
         private const string UsersCollectionName = "Users";
+        private const string RecordsCollectionName = "Records";
         private IMongoDatabase Database;
         private IMongoCollection<User> UsersCollection;
+        private IMongoCollection<Record> RecordsCollection;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeDatabase();
             InitializeCollections();
-            View.Content = new HomePage(this, UsersCollection);
+            View.Content = new HomePage(this, UsersCollection, RecordsCollection);
         }
 
         private void InitializeDatabase()
@@ -32,6 +34,7 @@ namespace ExpenseManager
         private void InitializeCollections()
         {
             UsersCollection = Database.GetCollection<User>(UsersCollectionName);
+            RecordsCollection = Database.GetCollection<Record>(RecordsCollectionName);
         }
     }
 }
