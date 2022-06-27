5142ef21822cbcf9b908d0ac8aa932341bfe4cf7
Rugilė Tolevičiūtė <rugile4@gmail.com>
2022-06-27 14:51:21 +0300

    Refactor SignInStepDefinitions

--- BDDTests/StepDefinitions/SignInStepDefinitions.cs
+++ BDDTests/StepDefinitions/SignInStepDefinitions.cs
@@ -1,26 +1,16 @@
 ﻿using BDDTests.Drivers;
-using ExpenseManager.DatabaseModels;
 using MongoDB.Driver;
 
 namespace BDDTests.StepDefinitions
 {
     [Binding]
-    public sealed class SignInStepDefinitions
+    public sealed class SignInStepDefinitions : StepDefinitionsBase
     {
-        private const int MillisecondsDelay = 2000;
-        private const string ConnectionString = "mongodb://127.0.0.1:27017";
-        private const string DatabaseName = "ExpenseManagerDB";
-        private const string UsersCollectionName = "Users";
-        private readonly List<User> Users;
         private readonly SignInForm SignInForm;
 
-        public SignInStepDefinitions(SignInForm signInForm)
+        public SignInStepDefinitions(SignInForm signInForm) : base()
         {
             SignInForm = signInForm;
-            var mongoClient = new MongoClient(ConnectionString);
-            var database = mongoClient.GetDatabase(DatabaseName);
-            var usersCollection = database.GetCollection<User>(UsersCollectionName);
-            Users = usersCollection.Find(_ => true).ToList();
         }
 
         [Given(@"the sign in form is loaded")]
